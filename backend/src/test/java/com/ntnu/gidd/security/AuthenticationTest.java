fdd46c484b45c2b0ee2c48eeefadce1b3535ebdb
Eirik Steira <eirsteir@stud.ntnu.no>
2021-04-14 12:47:39 +0200

    Implement basic login sanity check test

--- backend/src/test/java/com/ntnu/gidd/security/AuthenticationTest.java
+++ backend/src/test/java/com/ntnu/gidd/security/AuthenticationTest.java
@@ -1,26 +1,57 @@
 package com.ntnu.gidd.security;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
+@ContextConfiguration(classes = WebSecurity.class)
+//@TestPropertySource("classpath:application.properties")
 public class AuthenticationTest {
 
-    private static final String URI = "auth/";
+    private static final String URI = "/auth/";
 
     @Autowired
+    private WebApplicationContext context;
+
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
     @Test
     public void testLogin() throws Exception {
-        mockMvc.perform(post(URI + "login/"))
-        .andDo(print());
+        String loginJson = objectMapper.writeValueAsString("{\"username\":\"test\",\"password\":\"password123\"}");
+
+        mockMvc.perform(post(URI + "login/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(loginJson).with(csrf()))
+        .andDo(print())
+        .andExpect(status().isOk());
 
     }
 
