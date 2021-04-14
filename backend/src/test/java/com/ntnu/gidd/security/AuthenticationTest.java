2190a7920cf29cd9dc688451fe3094c98500dfff
Mads Lundegaard <mads.lundegaard@live.no>
2021-04-14 15:27:41 +0200

    Working on testing login

--- backend/src/test/java/com/ntnu/gidd/security/AuthenticationTest.java
+++ backend/src/test/java/com/ntnu/gidd/security/AuthenticationTest.java
@@ -3,33 +3,48 @@ package com.ntnu.gidd.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ntnu.gidd.controller.request.LoginRequest;
+import com.ntnu.gidd.model.User;
+import com.ntnu.gidd.repository.UserRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 @ContextConfiguration(classes = WebSecurity.class)
+@ActiveProfiles("test")
 public class AuthenticationTest {
-
+    
     private static final String URI = "/auth/";
-
+    private static final String email = "test@mail.com";
+    private static final String password = "password123";
+    
     @Autowired
     private WebApplicationContext context;
+    
+    @MockBean
+    private UserRepository userRepository;
 
     private MockMvc mockMvc;
 
@@ -39,6 +54,13 @@ public class AuthenticationTest {
 
     @BeforeEach
     public void setup() {
+        User testUser = new User();
+
+        testUser.setEmail(email);
+        testUser.setPassword(password);
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(testUser));
+        
+        
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .apply(springSecurity())
@@ -47,7 +69,10 @@ public class AuthenticationTest {
 
     @Test
     public void testLogin() throws Exception {
-        LoginRequest loginRequest = new LoginRequest("test@mail.com", "password123");
+    
+        System.out.println(userRepository.findByEmail(email));
+        
+        LoginRequest loginRequest = new LoginRequest(email, password);
         String loginJson = objectMapper.writeValueAsString(loginRequest);
 
         mockMvc.perform(post(URI + "login")
@@ -57,5 +82,10 @@ public class AuthenticationTest {
         .andExpect(status().isOk());
 
     }
-
+    
+    @Test
+    public void heii() throws Exception {
+        mockMvc.perform(get("/").with(user("user")));
+    }
+    
 }
