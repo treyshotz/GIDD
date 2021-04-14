2d1a6f12d6362afd0aec5b7e14cb5af5ab3b59ac
Eirik Steira <eirsteir@stud.ntnu.no>
2021-04-14 13:32:33 +0200

    Fix missing jwtConfig instance in JWT filters

--- backend/src/test/java/com/ntnu/gidd/security/AuthenticationTest.java
+++ backend/src/test/java/com/ntnu/gidd/security/AuthenticationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
@@ -34,6 +35,7 @@ public class AuthenticationTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
 
     @BeforeEach
     public void setup() {
