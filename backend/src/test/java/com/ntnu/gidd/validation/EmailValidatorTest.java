faffa469da56baaab3a337850d212632662ac2ba
Mads Lundegaard <mads.lundegaard@live.no>
2021-04-16 09:45:11 +0200

    Added tests for email validation

--- backend/src/test/java/com/ntnu/gidd/validation/EmailValidatorTest.java
+++ backend/src/test/java/com/ntnu/gidd/validation/EmailValidatorTest.java
@@ -1,4 +1,36 @@
 package com.ntnu.gidd.validation;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.apache.commons.validator.routines.EmailValidator;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
+
+@SpringBootTest(webEnvironment = MOCK)
+@AutoConfigureMockMvc
 public class EmailValidatorTest {
+	
+	@Test
+	public void testWithValidEmail() {
+		EmailValidator validator = EmailValidator.getInstance();
+		List<String> messages = Arrays.asList("test@mail.no", "test@mail.no.in", "test.testesen@mail.no", "test#test@mail.no");
+		messages.forEach(message -> {
+			assertTrue(validator.isValid(message));
+		});
+	}
+	
+	@Test
+	public void testWithInvalidEmail() {
+		EmailValidator validator = EmailValidator.getInstance();
+		List<String> messages = Arrays.asList("testmail.no" , "test@test", "test.no", "@test");
+		messages.forEach(message -> {
+			assertFalse(validator.isValid(message));
+		});
+	}
 }
